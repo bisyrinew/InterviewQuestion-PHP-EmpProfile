ce1e620ef3159558880e2e544be5503e5a7a8208
Bisyri <varco.enter@gmail.com>
2022-10-27 18:28:52 +0800

    first commit

---
+++ form-check.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+Name : <?php echo $_POST["name"]; ?><br>
+Gender : <?php echo $_POST["gender"]; ?> <br>
+Marital Status : <?php echo $_POST["marital"]; ?><br>
+Phone No : <?php echo $_POST["number"]; ?> <br>
+Email : <?php echo $_POST["email"]; ?><br>
+Address : <?php echo $_POST["address"]; ?> <br>
+Date of Birth : <?php echo $_POST["date_of_birth"]; ?><br>
+Hire Date : <?php echo $_POST["date_of_hiring"]; ?> <br>
+Department : <?php echo $_POST["department"]; ?><br>
+Nationality : <?php echo $_POST["nationality"]; ?> <br>
+
+</body>
+</html>
